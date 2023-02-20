c251f9868aa6ac1c8cd2d72187aaa6cf5518af40
Till Steinbach <steinbach.till@gmx.de>
2023-02-20 09:09:30 +0100

    update requirements

--- weconnect_cli/weconnect_cli_base.py
+++ weconnect_cli/weconnect_cli_base.py
@@ -138,7 +138,7 @@ def main():  # noqa: C901 # pylint: disable=too-many-statements,too-many-branche
         for handler in logging.root.handlers:
             handler.addFilter(util.DuplicateFilter())
 
-    ascii_magic.init_terminal()
+    #ascii_magic.init_terminal()
 
     username = None
     password = None
